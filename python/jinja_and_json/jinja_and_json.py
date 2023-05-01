0a8c4f33cb0c88ef6b1c93e0582257c2d1618559
David Bray <he157349@health.wa.gov.au>
2023-05-01 19:23:17 +0800

    Add argument to py script

--- python/jinja_and_json/jinja_and_json.py
+++ python/jinja_and_json/jinja_and_json.py
@@ -1,29 +1,58 @@
-# 
-# Python app to create SQL statements 
-# from project requirements
+#!/usr/bin/python
 #
+#-----------------------------------------------------------
+# Description : Python app to create SQL statements 
+#               from project requirements in JSON docs
+#  
+# Author      : Dave B  
+# Date        : AUG-2017
+# -----------------------------------------------------------
+
+
+# -----------------------------------------------------------
+# Libraries
+# -----------------------------------------------------------
 
 import os
 import json
 from jinja2 import Environment, FileSystemLoader
+import argparse
+
+# -----------------------------------------------------------
+# Functions
+# -----------------------------------------------------------
+
+def fn_parse_args():
+
+    """Parse arguments supplied to this script
+    
+    Args:
+        None
+        
+    Returns:
+        args : object containing parsed information
+    """
+
+    # Parse arguments supplied by the user
+    # for use later in this script
+    
+    parser = argparse.ArgumentParser(description='Create SQL from project JSON file')
+    
+    parser.add_argument( dest='json_filename'
+                        ,action='store'
+                        ,help='Filename of the JSON document')
+
+    args = parser.parse_args()     
+    #v_inifile = args.inifilename
+    return args
 
 # Opening JSON file
-f = open('201709.01_001_project.json')
-  
+f = open(args.json_filename)
+
 # returns JSON object as a dictionary
 data = json.load(f)
   
 project = data['project']
-
-#where_clauses = [{"field_name":"DateOfDiagnosis", "value":"20100101", "field_data_type":"date"  ,"is_list":"FALSE", "condition_type":">="  }
-#                ,{"field_name":"DateOfDiagnosis", "value":"20221231", "field_data_type":"date"  ,"is_list":"FALSE", "condition_type":"<="  }
-#                ,{"field_name":"Sex"            , "value":"2"       , "field_data_type":"string"  ,"is_list":"FALSE", "condition_type":"="  }
-#                ,{"field_name":"behaviour"      , "value":['1','2'] , "field_data_type":"string","is_list":"TRUE"  , "condition_type":"="}
-#                ,{"field_name":"icd_code"       , "value":["C56","C48.1","C48.2","C48.8","C57"]
-#                                                                       , "field_data_type":"string","is_list":"TRUE"  , "condition_type":"like"}
-#                ]
-#where_clauses = [{'field_name': 'DateOfDiagnosis', 'value': '20100101', 'field_data_type': 'date', 'is_list': 'FALSE', 'condition_type': '>='}, {'field_name': 'DateOfDiagnosis', 'value': '20221231', 'field_data_type': 'date', 'is_list': 'FALSE', 'condition_type': '<='}, {'field_name': 'Sex', 'value': '2', 'field_data_type': 'string', 'is_list': 'FALSE', 'condition_type': '='}, {'field_name': 'behaviour', 'value': ['1', '2'], 'field_data_type': 'string', 'is_list': 'TRUE', 'condition_type': '='}, {'field_name': 'icd_code', 'value': ['C56', 'C48.1', 'C48.2', 'C48.8', 'C57'], 'field_data_type': 'string', 'is_list': 'TRUE', 'condition_type': 'like'}]
-#where_clauses = [{"field_name": "DateOfDiagnosis", "value": "20100101", "field_data_type": "date", "is_list": "FALSE", "condition_type": ">="}, {"field_name": "DateOfDiagnosis", "value": "20221231", "field_data_type": "date", "is_list": "FALSE", "condition_type": "<="}, {"field_name": "Sex", "value": "2", "field_data_type": "string", "is_list": "FALSE", "condition_type": "="}, {"field_name": "behaviour", "value": ["1", "2"], "field_data_type": "string", "is_list": "TRUE", "condition_type": "="}, {"field_name": "icd_code", "value": ["C56", "C48.1", "C48.2", "C48.8", "C57"], "field_data_type": "string", "is_list": "TRUE", "condition_type": "like"}]
 
 env = Environment(loader=FileSystemLoader('templates'), lstrip_blocks=True,trim_blocks=True)
 template = env.get_template('cohort_select_sql.txt')
