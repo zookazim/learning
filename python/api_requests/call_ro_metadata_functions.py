d9f1944a835730018da0225a94f224c34ded68ed
David Bray <he157349@health.wa.gov.au>
2023-06-09 12:50:36 +0800

    Update to azure function

--- python/api_requests/call_ro_metadata_functions.py
+++ python/api_requests/call_ro_metadata_functions.py
@@ -1,13 +1,14 @@
 
-
+import sys
 import requests
 
 url = "http://localhost:7071/api/AzFnProjSqlGenerator"
 
-querystring = {"name":"Dave"}
+querystring = sys.stdin.readlines()
 
 response = requests.request("GET", url, params=querystring)
 
 print(response.text)
 print("Response code:{}".format(response.status_code))
+
 
